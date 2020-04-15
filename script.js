aaf2195460d9178d3e950e2d7e604d1494bb5a37
kross <662242@mail.ru>
2020-04-15 23:15:29 +0500

    54 lesson14

--- script.js
+++ script.js
@@ -3,36 +3,54 @@ let headerInput = document.querySelector('.header-input'),
     Add = document.querySelector('#add'),
     domic = document.querySelector('.domic');
 
- class DomElement {
-    element;
-    
+class DomElement {
     constructor(nameClass, height, width, bg, fontSize) {
-
-        if (nameClass[0] === '.') {
-            let cla = nameClass.slice(1);
-            this.element = document.createElement('div'); //document.createElement('li');
-            this.element.classList.add(cla);
-            this.element.style.height = height;
-            this.element.style.width = width;
-            this.element.style.backgroundColorg = bg;
-            
-    //console.log('this.element: ', this.element);
-
-        } else if (nameClass[0] === '#') {
-            let atr = nameClass.slice(1);
-            this.element = document.createElement('p'); //newElement.setAttribute('id','idName')
-            this.element.setAttribute('id', atr);
-            this.element.style.fontSize = fontSize;
-            this.element.style.bg = bg;
+        this.selector = nameClass
+        this.height = height;
+        this.width = width;
+        this.bg = bg;
+        this.fontSize = fontSize;
+    }
+     tochka  () {
+        let cla =  this.selector.slice(1);
+        let element = document.createElement('div'); //document.createElement('li');
+        element.classList.add(cla);
+        element.style.cssText = ` 
+            height: ${this.height}px;
+            width: ${this.width}px;
+            background: ${this.bg}`;
+            console.log('element: ', element);
+            return element;
+    }
+    netochka  () {
+        let atr =  this.selector.slice(1);
+        let element = document.createElement('p'); //newElement.setAttribute('id','idName')
+        element.setAttribute('id', atr);
+        element.style.cssText = ` 
+            height: ${height}px;
+            width: ${width}px;
+            background: ${bg};
+            font-size: ${fontSize}px`;
+    }
+    dif  () {
+        if (this.selector[0] === '.') {
+            this.tochka();
+        } else if (this.selector[0] === '#') {
+        this.netochka();
         }
     }
 };
 
+DomElement.prototype
 let height = '80px';
 let width = '80px';
 let bgd = '#444';
 let palas = '.hai';
-
-let dome = new DomElement(palas, height, width, bgd);
+let  fontSize= '14';
+let dome = new DomElement(palas, height, width, bgd, fontSize);
 console.log('dome: ', dome);
-domic.append(dome);
+let par=dome.dif();
+console.log('par: ', par);
+
+console.log('domic: ', domic);
+domic.append(par);
