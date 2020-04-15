7ed25db1247020ce62fc1b316c004e0e88638351
kross <662242@mail.ru>
2020-04-16 01:31:09 +0500

    исправил lesson14

--- script.js
+++ script.js
@@ -1,62 +1,44 @@
-'use strict';
-let headerInput = document.querySelector('.header-input'),
-    Add = document.querySelector('#add'),
-    domic = document.querySelector('.domic');
-
 class DomElement {
-    element;
-    constructor(nameClass, height, width, bg, fontSize) {
-        this.selector = nameClass
+
+    constructor(selector, height, width, bg, fontSize) {
+        this.selector = selector;
         this.height = height;
         this.width = width;
         this.bg = bg;
         this.fontSize = fontSize;
     }
-     tochka  () {
-        let cla =  this.selector.slice(1);
-        this.element = document.createElement('div'); //document.createElement('li');
-        this.element.classList.add(cla);
-        this.element.style.cssText = ` 
-            height: ${this.height}px;
-            width: ${this.width}px;
-            background: ${this.bg}`;
-            console.log('element: ', this.element);
-            return this.element;
-            
-    }
-    netochka  () {
-        let atr =  this.selector.slice(1);
-        this.element = document.createElement('p'); //newElement.setAttribute('id','idName')
-        this.element.setAttribute('id', atr);
-        this.element.style.cssText = ` 
-            height: ${this.height}px;
-            width: ${this.width}px;
-            background: ${this.bg};
-            font-size: ${this.fontSize}px`;
-
-            return this.element;
-        }
-    dif  () {
-        if (this.selector[0] === '.') {
-            let div=this.tochka();
-       return div;
-        } else if (this.selector[0] === '#') {
-            let pi=  this.netochka();
-            return pi;
-        }
-    }
 };
-
-DomElement.prototype
+DomElement.prototype.render = function () {
+    let bodyElement;
+    if (this.selector[0] === '.') {
+        let diV = document.createElement('div');
+        diV.textContent = 'Our Class' + this.selector.slice(1);
+        diV.classList.add(this.selector.slice(1));
+        document.body.appendChild(diV);
+        bodyElement = (diV);
+        console.log('bodyElement: ', bodyElement);
+    }
+    if (this.selector[0] === '#') {
+        let lem = document.createElement('p');
+        lem.textContent = 'Our Class' + this.selector.slice(1);
+        lem.id = this.selector.slice(1);
+        document.body.appendChild(lem);
+        bodyElement = (lem);
+    }
+    bodyElement.style.cssText = `
+    
+heigth:${this.height}px;
+width:${this.width}px;
+background-color:${this.bg}px;
+fontSize:${this.fontSize}px;
+`;
+console.log('bodyElement: ', bodyElement);
+};
 let height = '80px';
 let width = '80px';
-let bgd = '#444';
-let palas = '#hai';
-let  fontSize= '14';
-let dome = new DomElement(palas, height, width, bgd, fontSize);
+let bg = '#444';
+let palas = '.hai';
+let fontSize = '14';
+let dome = new DomElement('.hai','80px','80px', '#444', '14');
 
-let par = dome.dif();
-
-console.log('par: ', par);
-
-domic.append(par);
+dome.render();
